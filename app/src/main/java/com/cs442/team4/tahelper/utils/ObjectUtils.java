746df58d2448aa64d7fce897d4a2b555abd72003
Aditya Jadhav <aditya.jadhav27@gmail.com>
2016-11-05 09:44:00 -0500

    Utility for NULL checking

---
+++ app/src/main/java/com/cs442/team4/tahelper/utils/ObjectUtils.java
@@ -0,0 +1,34 @@
+package com.cs442.team4.tahelper.utils;
+
+import java.util.Collection;
+
+/**
+ * Created by neo on 05-11-2016.
+ */
+
+final public class ObjectUtils {
+
+
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    public static boolean isNotEmpty(String value) {
+        return !isEmpty(value);
+    }
+
+    public static boolean isEmpty(String value) {
+        if (value != null && value.length() > 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isEmpty(Collection<?> collection) {
+        if (collection != null && collection.size() > 0) {
+            return false;
+        }
+        return true;
+    }
+
+}
