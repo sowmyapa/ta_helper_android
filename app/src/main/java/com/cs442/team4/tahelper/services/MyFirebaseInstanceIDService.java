4037692e2e355f0d246e9a1c09cf088271aa1c8b
Aditya Jadhav <aditya.jadhav27@gmail.com>
2016-11-12 17:14:46 -0600

    Set Up a Firebase Cloud Messaging Client

---
+++ app/src/main/java/com/cs442/team4/tahelper/services/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.cs442.team4.tahelper.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by neo on 12-11-2016.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
