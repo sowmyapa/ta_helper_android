1a81815b3ac149b5ae23069364b149c82956a5b8
Aditya Jadhav <aditya.jadhav27@gmail.com>
2016-11-12 20:15:42 -0600

    send registration token to server

--- app/src/main/java/com/cs442/team4/tahelper/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/cs442/team4/tahelper/services/MyFirebaseMessagingService.java
@@ -44,7 +44,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Firebase Push Notification")
+                .setContentTitle("TA Helper")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
